1248c8cab4d8bebab1f4d9e3b288f6895dcbfe03
Scott Weeden <sweeden@ttu.edu>
2025-09-23 22:31:23 -0500

    Implemented NFA

--- automata-theory-project/src/automata/nfa.py
+++ automata-theory-project/src/automata/nfa.py
@@ -10,5 +10,25 @@ class NFA:
     
     def accepts(self, string):
         """Check if string is accepted by NFA"""
-        # TODO: Implement NFA acceptance
-        pass
+        def epsilon_closure(states):
+            closure = set(states)
+            stack = list(states)
+            while stack:
+                state = stack.pop()
+                if (state, '') in self.transitions:
+                    for next_state in self.transitions[(state, '')]:
+                        if next_state not in closure:
+                            closure.add(next_state)
+                            stack.append(next_state)
+            return closure
+        
+        current_states = epsilon_closure({self.start_state})
+        
+        for symbol in string:
+            next_states = set()
+            for state in current_states:
+                if (state, symbol) in self.transitions:
+                    next_states.update(self.transitions[(state, symbol)])
+            current_states = epsilon_closure(next_states)
+        
+        return bool(current_states & self.accept_states)
